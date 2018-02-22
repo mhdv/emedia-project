2ec8a550327702c083b22a5b3d71876183602ab0
Rafalo1212 <a.rivendare@o2.pl>
2018-02-22 13:19:42 +0100

    riff

--- test.c
+++ test.c
@@ -2,9 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]){
-	
+typedef struct header
+{
+  char riff[4];
+  int fsize;
+  char file_type[4];
+  char format_chunk[4];
+  int chunk_size;
+  int format_type;
+  int channel_number;
+  int samples_per_second;
+  int bits_per_second;
+  int audio_type;
+  int bits_per_sample;
+  char data[4];
+  int data_length;
+}hdr;
+
+
+int main(int argc, char *argv[])
+{
 	int c;
+	
+	
 	if(!argv[1]){
 		printf("Error: first argument not found.\n");
 		return 0;
@@ -18,18 +38,21 @@ int main(int argc, char *argv[]){
 		}
 			// BODY OF APP
 
+		hdr fheader;
+		int fheader_size=sizeof(fheader);
 			FILE *file;
 			if((file=fopen(argv[1], "r")) == NULL){
 				printf("Error");
 				return 0;
 			}
-			/*if(file){
-				while((c = getc(file)) != EOF){
-					putchar(c);
-				}
-				}*/
+			fread(&fheader, fheader_size, 1, file);
+			int file_length=sizeof(file);
+			fclose(file);
 
-			return 0;
+			printf("RIFF: %c%c%c%c\n", fheader.riff[0], fheader.riff[1], fheader.riff[2], fheader.riff[3]);
+			
+		        
 	}
+	
 	return 0;
 }
